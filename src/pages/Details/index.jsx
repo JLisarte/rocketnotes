bec12acb9330d618fa29c4fe7d7f81816136ab96
JLisarte <juliano.lisarte@gmail.com>
2024-01-19 12:18:39 -0300

    Refactor section and details page styles

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,26 +1,47 @@
-import { Container, Links } from "./styles"
+import { Container, Links, Content } from "./styles"
 
 import { Header } from "../../components/Header"
 import { Button } from "../../components/Button"
+import { ButtonText } from "../../components/ButtonText"
 import { Section } from "../../components/Section"
+import { Tag } from "../../components/Tag"
 
 export function Details() {
   return (
     <Container>
       <Header />
 
-      <Section title="Links úteis" >
-        <Links>
-          <li>
-            <a href="#">https://www.rocketseat.com.br</a>
-          </li>
-          <li>
-            <a href="#">https://www.rocketseat.com.br</a>
-          </li>
-        </Links>
-      </Section>
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota" />
 
-      <Button title="Voltar" />
+          <h1>
+            Introdução ao React
+          </h1>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut labore aut  ipsa necessitatibus quas id ullam, ratione non vero sit voluptates doloremque in magnam velit fuga quos itaque sapiente aspernatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
+          </p>
+
+          <Section title="Links úteis">
+            <Links>
+              <li>
+                <a href="#">https://www.rocketseat.com.br</a>
+              </li>
+              <li>
+                <a href="#">https://www.rocketseat.com.br</a>
+              </li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="express" />
+            <Tag title="nodejs" />
+          </Section>
+
+          <Button title="Voltar" />
+        </Content>
+      </main>
     </Container>
   )
 }
