eb96a82e30241141a6e3d60307357fc6aec264ed
JLisarte <juliano.lisarte@gmail.com>
2024-01-22 08:03:57 -0300

    Add "Criar nota" button to NewNote component

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -48,13 +48,24 @@ export const Menu = styled.ul`
 `
 
 export const Search = styled.div`
-
+  grid-area: search;
 `
 
 export const Content = styled.div`
-
+  grid-area: content;
 `
 
-export const NewNote= styled.button`
+export const NewNote = styled.button`
+  grid-area: newnote;
 
+  background: ${({ theme }) => theme.COLORS.ORANGE};
+  border: none;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  svg {
+    margin-right: 8px;
+  }
 `
